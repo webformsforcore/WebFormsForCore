0455b92a8a932b50b3dbfd87b7c2957dd8b75abf
Simon Jakob Egli <simon.jakob.egli@gmail.com>
2024-07-30 11:34:53 +0200

    Working on BuildDate

---
+++ src/WebFormsCore.Configuration/System/System.Configuration/BuildDateAttribute.cs
@@ -0,0 +1,13 @@
+﻿using System.Globalization;
+using System;
+
+[AttributeUsage(AttributeTargets.Assembly)]
+internal class BuildDateAttribute : Attribute
+{
+	public BuildDateAttribute(string value)
+	{
+		DateTime = DateTime.ParseExact(value, "yyyyMMddHHmmss", CultureInfo.InvariantCulture, DateTimeStyles.None);
+	}
+
+	public DateTime DateTime { get; }
+}
