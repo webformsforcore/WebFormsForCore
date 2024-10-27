9ef310939c34afd499888a39cc2d40aead102f29
Simon Jakob Egli <simon.jakob.egli@gmail.com>
2024-10-27 04:52:08 +0100

    Impreved CreatePackages

--- CreatePackages.bat
+++ CreatePackages.bat
@@ -1,33 +1,37 @@
 SET PackageVersion=1.1.0-beta
+SET /p ApiKey=<NugetApiKey.txt
 
 cd  src
+dotnet build
 cd WebFormsForCore.Build
-dotnet pack --include-symbols -o ..\..\nupkg
+dotnet pack --include-symbols -o ..\..\nupkg --no-build
 cd ..\WebFormsForCore.Configuration
-dotnet pack --include-symbols -o ..\..\nupkg
+dotnet pack --include-symbols -o ..\..\nupkg --no-build
 cd ..\WebFormsForCore.Drawing
-dotnet pack --include-symbols -o ..\..\nupkg
+dotnet pack --include-symbols -o ..\..\nupkg --no-build
 cd ..\WebFormsForCore.Serialization.Formatters
-dotnet pack --include-symbols -o ..\..\nupkg
+dotnet pack --include-symbols -o ..\..\nupkg --no-build
 cd ..\WebFormsForCore.Web
-dotnet pack WebFormsForCore.Web.csproj --include-symbols -o ..\..\nupkg
+dotnet pack WebFormsForCore.Web.csproj --include-symbols -o ..\..\nupkg --no-build
 cd ..\WebFormsForCore.Web.ApplicationServices
-dotnet pack --include-symbols -o ..\..\nupkg
+dotnet pack --include-symbols -o ..\..\nupkg --no-build
 cd ..\WebFormsForCore.Web.Extensions
-dotnet pack --include-symbols -o ..\..\nupkg
+dotnet pack --include-symbols -o ..\..\nupkg --no-build
 cd ..\WebFormsForCore.Web.Infrastructure
-dotnet pack --include-symbols -o ..\..\nupkg
+dotnet pack --include-symbols -o ..\..\nupkg --no-build
 cd ..\WebFormsForCore.Web.Optimization
-dotnet pack WebFormsForCore.Web.Optimization.csproj --include-symbols -o ..\..\nupkg
+dotnet pack WebFormsForCore.Web.Optimization.csproj --include-symbols -o ..\..\nupkg --no-build
 cd ..\WebFormsForCore.Web.Optimization.WebForms
-dotnet pack --include-symbols -o ..\..\nupkg
+dotnet pack --include-symbols -o ..\..\nupkg --no-build
 cd ..\WebFormsForCore.Web.RegularExpressions
-dotnet pack --include-symbols -o ..\..\nupkg
+dotnet pack --include-symbols -o ..\..\nupkg --no-build
 cd ..\WebFormsForCore.Web.Services
-dotnet pack --include-symbols -o ..\..\nupkg
+dotnet pack --include-symbols -o ..\..\nupkg --no-build
 cd ..\WebFormsForCore.WebGrease
-dotnet pack WebFormsForCore.WebGrease.csproj --include-symbols -o ..\..\nupkg
+dotnet pack WebFormsForCore.WebGrease.csproj --include-symbols -o ..\..\nupkg --no-build
 cd ..\..
 
+cd nuget
 
-
+for /r %i in (*.nupkg) do dotnet nuget push %i --api-key %ApiKey%
+for /r %i in (*.snupkg) do dotnet nuget push %i --api-key %ApiKey% 
