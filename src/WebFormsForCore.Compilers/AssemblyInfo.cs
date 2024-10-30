3c7f71d2294eb012c5cc5d2c2257b859174f49e3
Simon Jakob Egli <simon.jakob.egli@gmail.com>
2024-10-30 06:18:29 +0100

    Fix in TestApp & Compilers

---
+++ src/WebFormsForCore.Compilers/AssemblyInfo.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Reflection;
+using System.Resources;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Security;
+
+[assembly: ComVisible(false)]
+[assembly: CLSCompliant(true)]
+[assembly: AllowPartiallyTrustedCallers(PartialTrustVisibilityLevel = PartialTrustVisibilityLevel.NotVisibleByDefault)]
+[assembly: SecurityRules(SecurityRuleSet.Level1, SkipVerificationInFullTrust = true)]
+[assembly: AssemblyTitle("EstrellasDeEsperanza.WebFormsForCore.Compilers.dll")]
+[assembly: AssemblyDescription("EstrellasDeEsperanza.WebFormsForCore.Compilers.dll")]
+[assembly: AssemblyDefaultAlias("EstrellasDeEsperanza.WebFormsForCore.Compilers.dll")]
+[assembly: AssemblyCompany("Estrellas de Esperanza")]
+[assembly: AssemblyProduct("EstrellasDeEsperanza.WebFormsForCore")]
+[assembly: AssemblyCopyright("© Estrellas de Esperanza. All rights reserved.")]
+[assembly: AssemblyFileVersion("8.0.0.0")]
+[assembly: AssemblyInformationalVersion("8.0.0.0")]
+[assembly: SatelliteContractVersion("8.0.0.0")]
+[assembly: NeutralResourcesLanguage("en-US")]
+[assembly: AssemblyDelaySign(false)]
+[assembly: AssemblyKeyFile(@"..\SigningKey\WebFormsForCore.snk")]
+[assembly: DefaultDllImportSearchPaths(DllImportSearchPath.System32 | DllImportSearchPath.AssemblyDirectory)]
+[assembly: AssemblyVersion("8.0.0.0")]
