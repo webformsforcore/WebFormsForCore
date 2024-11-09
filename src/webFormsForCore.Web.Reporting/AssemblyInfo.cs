886cfacda1f411c46a0d79acfa1c61a4c6dc21b5
Simon Jakob Egli <simon.jakob.egli@gmail.com>
2024-11-09 20:36:56 +0100

    Added Reporting

---
+++ src/webFormsForCore.Web.Reporting/AssemblyInfo.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Reflection;
+using System.Resources;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Security;
+using System.Web.UI;
+
+[assembly: AssemblyCompany("Estrellas de Esperanza")]
+[assembly: AssemblyProduct("EstrellasDeEsperanza.WebFormsForCore")]
+[assembly: AssemblyDelaySign(false)]
+[assembly: AssemblyKeyFile(@"..\SigningKey\WebFormsForCore.snk")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AllowPartiallyTrustedCallers]
+[assembly: SecurityCritical]
+[assembly: TagPrefix("Microsoft.Reporting.WebForms", "rsweb")]
+[assembly: AssemblyDefaultAlias("Microsoft.ReportViewer.WebForms.dll")]
+[assembly: ComVisible(false)]
+[assembly: CLSCompliant(true)]
+
+[assembly: AssemblyTitle("Microsoft.ReportViewer.WebForms.dll")]
+[assembly: AssemblyDescription("Microsoft.ReportViewer.WebForms.dll")]
+[assembly: AssemblyFileVersion("10.0.40219.1")]
+//[assembly: InternalsVisibleTo("Microsoft.ReportViewer.WebDesign, PublicKey=002400000480000094000000060200000024000052534131000400000100010007D1FA57C4AED9F0A32E84AA0FAEFD0DE9E8FD6AEC8F87FB03766C834C99921EB23BE79AD9D5DCC1DD9AD236132102900B723CF980957FC4E177108FC607774F29E8320E92EA05ECE4E821C0A5EFE8F1645C4C0C93C1AB99285D622CAA652C1DFAD63D745D6F2DE5F17E5EAF0FC4963D261C8A12436518206DC093344D5AD293")]
+[assembly: SatelliteContractVersion("10.0.0.0")]
+[assembly: AssemblyInformationalVersion("10.0.40219.1")]
+[assembly: NeutralResourcesLanguage("en-US")]
+[assembly: AssemblyVersion("10.0.0.0")]
