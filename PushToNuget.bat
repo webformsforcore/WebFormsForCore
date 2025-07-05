8eb83cd4212301fb34aa661ef6a1c7cfe0046c57
Simon Jakob Egli <simon.jakob.egli@gmail.com>
2025-07-05 22:34:33 +0200

    Fix

--- PushToNuget.bat
+++ PushToNuget.bat
@@ -3,7 +3,7 @@ SET /p ApiKey=<NugetApiKey.txt
 cd nupkg
 
 for /r %%i in (*.nupkg) do (
-    dotnet nuget push %%i --api-key %ApiKey% -s https://api.nuget.org/v3/index.json
+    dotnet nuget push %%i --api-key %ApiKey% -s https://api.nuget.org/v3/index.json --skip-duplicate
 )
 REM for /r %%i in (*.snupkg) do (
 REM    dotnet nuget push %%i --api-key %ApiKey% -s https://api.nuget.org/v3/index.json
