4735b732087bd0caab3840f2b1d78c110f6e06bc
Simon Jakob Egli <simon.jakob.egli@gmail.com>
2026-06-27 23:56:59 +0200

    Added assembly info to AspNetCompiler

---
+++ src/WebFormsForCore.AspNetCompiler/Properties/AssemblyInfo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Reflection;
+using System.Resources;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Security;
+
+[assembly: ComVisible(false)]
+[assembly: CLSCompliant(true)]
+[assembly: AllowPartiallyTrustedCallers(PartialTrustVisibilityLevel = PartialTrustVisibilityLevel.NotVisibleByDefault)]
+[assembly: SecurityRules(SecurityRuleSet.Level1, SkipVerificationInFullTrust = true)]
+[assembly: AssemblyTitle("WebFormsForCore.AspNetCompiler")]
+[assembly: AssemblyDescription("WebFormsForCore.AspNetCompiler")]
+[assembly: AssemblyDefaultAlias("WebFormsForCore.AspNetCompiler")]
+[assembly: AssemblyProduct("WebFormsForCore")]
+[assembly: NeutralResourcesLanguage("en-US")]
+[assembly: AssemblyDelaySign(false)]
